04e4d924687c9dc04e9017e1b610b98814c79a67
Jonathan Perry <jpper@umich.edu>
2021-01-29 18:21:42 +0900

    Added a key to prevent an error

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -131,7 +131,7 @@ const ChatScreen = ({ navigation, route }) => {
                     <Text style={styles.receiverText}>{data.message}</Text>
                   </View>
                 ) : (
-                  <View style={styles.sender}>
+                  <View key={id} style={styles.sender}>
                     <Avatar
                       position="absolute"
                       rounded
